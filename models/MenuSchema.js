9867f85f6eca4944ba682887a1f2243dc214b05d
tonildo123 <tony81191@gmail.com>
2022-12-11 13:33:12 -0300

    se agrego archivos con el modelo de base de datos 13:30 - 11/12/2022

---
+++ models/MenuSchema.js
@@ -0,0 +1,36 @@
+// modelo base de datos schema de menu
+const mongoose =  require('mongoose')
+
+const MenuSchema = new mongoose.Schema({
+    nombre:{
+        type:String, 
+        require:true,
+        trim:true
+    },
+    estado:{
+        type:Boolean,// validacion de estado, true (disponible) o false (no disponible)
+        default:true,
+        require:true,
+
+    },
+    precio:{
+        type:Number, // validacion de precio, solo numeros
+        require:true,
+        trim:true
+        
+    },
+    detalle:{
+        type:String, // validacion de detalle, solo texto
+        require:true,
+        trim:true
+    },
+    categoria:{
+        type:String, // validacion de categoria, solo texto
+        require:true,
+        trim:true
+    }
+})
+
+const Menu = mongoose.model('Menu', MenuSchema);
+
+module.exports = Menu;
