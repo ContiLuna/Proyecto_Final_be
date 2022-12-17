e33f732e9f615a7cecb37ce1bbaa8134f0064f5a
Conti Luna <108755243+ContiLuna@users.noreply.github.com>
2022-12-17 20:14:15 -0300

    Se sube config cloudinary (#9)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 const app = express();
 const conectarDB = require("./database/db");
+const cloudinary= require("cloudinary").v2
 const router = require("./routes/index");
 
 
@@ -22,9 +23,16 @@ app.get("/",(req, res)=>{
     return res.send("holiwi")
 })
 
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.APY_KEY, 
+  api_secret: process.env.API_SECRET,
+});
+
 //ejecutar conexcion a DB
 conectarDB();
 
 app.listen(port, () => {
   console.log(`mi servidor esta funcionando en el puerto ${port}`);
 });
+
